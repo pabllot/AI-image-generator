e3a4f5b3533ae3c17a774ce29174178a87c46c07
Pabllot07 <pabllot07@outlook.com>
2023-01-22 11:47:31 -0300

    setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Link, Route, Routes} from 'react-router-dom'
+import CreatePost from './pages/CreatePost/CreatePost'
+import Home from './pages/Home/Home'
+
+import {logo} from './assets'
+import { Header, Image } from './styles'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Header>
+        <Link to="/">
+          <Image src={logo} alt='logo' />
+        </Link>
+      </Header>
+    </BrowserRouter>
   )
 }
 
---
+++ src/styles.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const Header = styled.header`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background: white;
+    padding: 1rem;
+    border-bottom: 1px solid #e6ebf4;
+
+
+    @media (max-width: 640px) {
+        padding: 1rem, 2rem;
+    }
+
+`
+
+export const Image = styled.img`
+    width: 7rem;
+    object-fit: contain;
+`
