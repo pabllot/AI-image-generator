c82fbbefca087c2c729b64b35a712f7c5eb9b099
Pabllot07 <pabllot07@outlook.com>
2023-01-22 18:36:00 -0300

    form field on the way

--- src/components/FormField/FormField.jsx
+++ src/components/FormField/FormField.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Container, BigContainer, Label, Button } from './styles'
 
-const FormField = () => {
+const FormField = ({ labelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe }) => {
   return (
-    <div>FormField</div>
+    <BigContainer>
+      <Container>
+        <Label htmlFor={name}>
+          {labelName}
+        </Label>
+        {isSurpriseMe && (
+          <Button
+            type='button'
+            onClick={handleSurpriseMe}
+          >
+            Surprise Me
+          </Button>
+        )}
+      </Container>
+    </BigContainer>
   )
 }
 
---
+++ src/components/FormField/styles.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const BigContainer = styled.div``
+
+export const Container = styled.div`
+    display: flex;
+    gap: 10px;
+    align-items: center;
+    margin-bottom: 8px;
+`
+
+export const Label = styled.label`
+    display: block;
+    font-size: 14px;
+    font-weight: 500;
+    color: gray;
+`
+
+export const Button = styled.button`
+    font-weight: 600;
+    font-size: 12px;
+    background: #ECECF1;
+    padding: 1px 2px;
+    border-radius: 5px;
+    color: black;
+    border: none;
+`
