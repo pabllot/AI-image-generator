82e0558c05c14581029bb9852a82e8b36a0dac3f
Pabllot07 <pabllot07@outlook.com>
2023-01-22 14:09:32 -0300

    renderCards done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,20 @@
 import {useState} from 'react'
-import { Section, Wrapper, H1, Paragraph, FormContainer, LoaderContainer, LoaderWrapper } from './styles'
+import { Section, Wrapper, H1, Paragraph, FormContainer, LoaderContainer, LoaderWrapper, H2, Span, GridContainer, Title } from './styles'
 import { Loader, Card, FormField } from '../../components'
 
+const RenderCards = ({data, title}) => {
+  if(data?.length > 0) {
+    return data.map((post) => <Card key={post._id} {...post} />)
+  }
+  return (
+    <Title>{title}</Title>
+  )
+}
+
 const Home = () => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null)
+  const [searchText, setSearchText] = useState('')
 
   return (
     <Section>
@@ -23,7 +33,24 @@ const Home = () => {
             <Loader/>
           </LoaderWrapper>
         ) : (
-          <></>
+          <>
+            {searchText && (
+            <H2>Showing results for <Span>{searchText}</Span></H2>
+            )}
+            <GridContainer>
+              {searchText ? (
+                <RenderCards
+                  data={[]}
+                  title="No search results found"
+                />
+              ) : (
+                <RenderCards 
+                  data={[]}
+                  title="No posts found"
+                />
+              )}
+            </GridContainer>
+          </>
         )}
       </LoaderContainer>
 
--- src/pages/Home/styles.jsx
+++ src/pages/Home/styles.jsx
@@ -34,5 +34,42 @@ export const LoaderWrapper = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    color: white
+    color: white;
+`
+export const H2 = styled.h2`
+    font-weight: 500;
+    color: #666e75;
+    margin-bottom: 1.2rem;
+    font-size: 1.2rem;
+`
+
+export const Span = styled.span`
+font-size: 1.2rem;
+color: #222328;
+`
+
+export const GridContainer = styled.div`
+        display: grid;
+        grid-template-columns: repeat(1, minmax(0, 1fr));
+        gap: 1.2rem;
+
+        @media (min-width: 1024px) {
+            grid-template-columns: repeat(4, minmax(0, 1fr));
+        }
+        @media (min-width: 640px) {
+            grid-template-columns: repeat(3, minmax(0, 1fr));
+        }
+        @media (min-width: 400px) {
+            grid-template-columns: repeat(2, minmax(0, 1fr));
+        }
+
+`
+
+export const Title = styled.h2`
+    margin-top: 1rem;
+    font-weight: 700;
+    color: #6449ff;
+    text-transform: uppercase;
+    font-size: 1rem;
+
 `
