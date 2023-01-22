c114c0a55b011a35e2e33dfaf9563a91171c6a15
Pabllot07 <pabllot07@outlook.com>
2023-01-22 18:15:05 -0300

    create-post on the way

---
+++ src/pages/CreatePost/styles.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+    max-width: 80rem;
+    margin: 0 auto;
+`
+
+export const Wrapper = styled.div``
+
+export const H1 = styled.h1`
+    font-weight: 700;
+    color: #222328;
+    font-size: 32px;
+`
+
+export const Paragraph = styled.p`
+    margin-top: 8.5px;
+    color: #666e75;
+    font-size: 16px;
+    max-width: 700px;
+
+`
+
+export const Form = styled.form`
+    margin-top: 4rem;
+    max-width: 30rem;
+`
+
+export const FormContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 5;
+`
+
