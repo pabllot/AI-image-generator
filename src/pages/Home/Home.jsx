e3d6dfe31804875dd5e58365b1cd2c40639b92d9
Pabllot07 <pabllot07@outlook.com>
2023-01-23 18:01:01 -0300

    fetching posts done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import { Section, Wrapper, H1, Paragraph, FormContainer, LoaderContainer, LoaderWrapper, H2, Span, GridContainer, Title } from './styles'
 import { Loader, Card, FormField } from '../../components'
 
@@ -15,6 +15,35 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null)
   const [searchText, setSearchText] = useState('')
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+
+        if(response.ok) {
+          const result = await response.json();
+
+          setAllPosts(result.data.reverse());
+        }
+
+      } catch (error) {
+        alert(error)
+
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchPosts();
+  }, [])
 
   return (
     <Section>
@@ -45,7 +74,7 @@ const Home = () => {
                 />
               ) : (
                 <RenderCards 
-                  data={[]}
+                  data={allPosts}
                   title="No posts found"
                 />
               )}
